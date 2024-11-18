6701dcf8120f112b2fc849065cbb02da8e2154c4
muhammadsufiyanbaig <BDA24S001@stu.smiu.edu.pk>
2024-11-18 13:00:49 +0500

    remove framer motion

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
@@ -15,11 +14,7 @@ export default function Header() {
 
   return (
     <header className="z-[999] relative">
-      <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border-2 border-white border-opacity-40 bg-transparent bg-opacity-80 shadow-xl shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-        initial={{ y: -100, x: "-50%", opacity: 0 }}
-        animate={{ y: 0, x: "-50%", opacity: 1 }}
-      ></motion.div>
+      <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border-2 border-white border-opacity-40 bg-transparent bg-opacity-80 shadow-xl shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75 transform -translate-x-1/2"></div>
 
       {/* Hamburger Menu Icon for Mobile */}
       <div className="fixed top-4 left-4 sm:hidden z-50">
@@ -47,12 +42,7 @@ export default function Header() {
       >
         <ul className="flex flex-col items-center justify-center h-full gap-y-5 text-[1.2rem] font-medium text-gray-500 dark:text-gray-400">
           {links.map((link) => (
-            <motion.li
-              className="relative"
-              key={link.hash}
-              initial={{ x: -100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-            >
+            <li className="relative" key={link.hash}>
               <Link
                 className={clsx(
                   "px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300",
@@ -68,20 +58,11 @@ export default function Header() {
                 }}
               >
                 {link.name}
-
                 {link.name === activeSection && (
-                  <motion.span
-                    className="bg-orange-500 p-3 rounded-full absolute inset-0 -z-10 dark:bg-orange-500"
-                    layoutId="activeSection"
-                    transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
-                    }}
-                  ></motion.span>
+                  <span className="bg-orange-500 p-3 rounded-full absolute inset-0 -z-10 dark:bg-orange-500"></span>
                 )}
               </Link>
-            </motion.li>
+            </li>
           ))}
         </ul>
       </nav>
@@ -90,11 +71,9 @@ export default function Header() {
       <nav className="hidden sm:flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-            <motion.li
+            <li
               className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
@@ -110,20 +89,11 @@ export default function Header() {
                 }}
               >
                 {link.name}
-
                 {link.name === activeSection && (
-                  <motion.span
-                    className="bg-orange-500 rounded-full absolute inset-0 -z-10 dark:bg-orange-500"
-                    layoutId="activeSection"
-                    transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
-                    }}
-                  ></motion.span>
+                  <span className="bg-orange-500 rounded-full absolute inset-0 -z-10 dark:bg-orange-500"></span>
                 )}
               </Link>
-            </motion.li>
+            </li>
           ))}
         </ul>
       </nav>
